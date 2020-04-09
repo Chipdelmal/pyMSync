78451f5873174a4f276f9796a437aa09e901c5db
chipdelmal <chipdelmal@gmail.com>
2020-04-09 09:27:54 -0700

    Changing the way we iterite through files.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import shutil
 import functions as fun
 from pathlib import Path
@@ -10,11 +11,21 @@ from pathlib import Path
 (IPTH, OPTH) = ('./playlists/', './out/')
 
 
-with open('{}/{}'.format(IPTH, pName)) as f:
-    # Read head line
-    head = f.readline()
+flines = Path('{}/{}'.format(IPTH, pName)).read_text().splitlines()
+
+# Read head line and count the songs number
+(head, flinesNum) = (flines[0], len(flines) - 1)
+(sInfo, sPath) = (
+        [flines[i] for i in range(1, flinesNum, 2)],
+        [flines[i] for i in range(2, flinesNum+1, 2)]
+    )
+if len(sPath) != len(sInfo):
+    sys.exit("There's a problem with the m3u (unequal ids to songs)")
+
+sNum = len(sInfo)
+for i in range(sNum):
     # Start reading playlist pairs
-    (info, path) = (fun.clnStr(f.readline()), fun.clnStr(f.readline()))
+    (info, path) = (fun.clnStr(sInfo[i]), fun.clnStr(sPath[i]))
     (fPath, fName) = (os.path.dirname(path), os.path.basename(path))
     if delFileTag:
         infPath = path.replace('file://', '')
@@ -26,5 +37,3 @@ with open('{}/{}'.format(IPTH, pName)) as f:
     oufPath = '{}/{}'.format(outPth, fName)
     # Copy file to the ouput folder
     dest = shutil.copyfile(infPath, oufPath)
-
-oufPath
