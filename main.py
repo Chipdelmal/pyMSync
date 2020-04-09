2468cf488019a69325335b78c3af7b745138e44d
chipdelmal <chipdelmal@gmail.com>
2020-04-09 08:59:08 -0700

    Script is copying files correctly

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import os
+import shutil
 import functions as fun
 from pathlib import Path
 
-(pName, fldLv) = ('CourteenersLive.m3u', -2)
+(pName, fldLv, delFileTag) = ('CourteenersLive.m3u', -2, True)
 (IPTH, OPTH) = ('./playlists/', './out/')
 
 
@@ -15,6 +16,15 @@ with open('{}/{}'.format(IPTH, pName)) as f:
     # Start reading playlist pairs
     (info, path) = (fun.clnStr(f.readline()), fun.clnStr(f.readline()))
     (fPath, fName) = (os.path.dirname(path), os.path.basename(path))
+    if delFileTag:
+        infPath = path.replace('file://', '')
+    else:
+        infPath = path
     # Clean and create folder path in output
     outPth = fun.genOutPth(fPath, OPTH, fldLv=-2)
     Path(outPth).mkdir(parents=True, exist_ok=True)
+    oufPath = '{}/{}'.format(outPth, fName)
+    # Copy file to the ouput folder
+    dest = shutil.copyfile(infPath, oufPath)
+
+oufPath
