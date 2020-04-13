993f1cdbc3c1dc32e439f582391253c5020770ed
chipdelmal <chipdelmal@gmail.com>
2020-04-13 12:50:38 -0700

    Adding single and batch to iTunes converter.

--- convItunesPlaylist.py
+++ convItunesPlaylist.py
@@ -9,6 +9,16 @@ oPth = '/media/hdd/Music/'
 bOld = '/Users/glados/Music/Music/Media.localized/'
 bNew = '/media/hdd/Music/'
 
+
+# #############################################################################
+# Single
+# #############################################################################
+pName = 'Mixtape001_ NewIndie.m3u'
+fun.fixPlistReference(iPth + pName, oPth, bOld, bNew, ClnPath=True)
+
+# #############################################################################
+# Batch
+# #############################################################################
 allPlsts = glob.glob(iPth + '*.m3u')
 for plst in allPlsts:
     fun.fixPlistReference(plst, oPth, bOld, bNew, ClnPath=True)
